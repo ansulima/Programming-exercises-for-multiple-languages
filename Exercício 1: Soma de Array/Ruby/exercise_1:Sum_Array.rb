817c2d6205f32468c62ad90fd3d758d0734eb579
Anderson Freitas <freitaslimaanderson@gmail.com>
2024-05-27 10:11:43 -0300

    refactor: Adding adding the question of the question

--- Exercício 1: Soma de Array/Ruby/exercise_1:Sum_Array.rb
+++ Exercício 1: Soma de Array/Ruby/exercise_1:Sum_Array.rb
@@ -1,3 +1,16 @@
+# Descrição: Escreva uma função que recebe um array de números e retorna a soma de todos os elementos.
+
+# Regra: Não utilize funções de bibliotecas que façam a soma diretamente
+
+# Input: [1, 2, 3, 4, 5]
+
+# Output: 15
+
+# Input: [-1, 1, 0, 5, -5]
+
+# Output: 0
+
+
 def sum_array(array)
     sum = 0
     array.each do |number|
